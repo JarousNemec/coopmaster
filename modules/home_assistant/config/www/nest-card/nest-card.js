1b3a4a80036f0ad536aa75b8b28805fd46ed88c7
JarousNemec <jarous.nemec@gmail.com>
2025-01-01 21:14:22 +0100

    deleded docker folder, finished setup of dashboard with all sensors, topics and mqtt publishers

--- modules/home_assistant/config/www/nest-card/nest-card.js
+++ modules/home_assistant/config/www/nest-card/nest-card.js
@@ -13,27 +13,40 @@ class NestCard extends HTMLElement {
             this.appendChild(card);
         }
 
+        const separator = ";";
         const logging = this.config.logging || false;
+
         const nests_in_row = this.config.nests_in_row || 0;
         const nests_in_column = this.config.nests_in_column || 0;
-        const separator = ";"
-        const entityId = this.config.entity;
-        const entity = hass.states[entityId];
+
+        const entity_nests_id = this.config.entity_nests;
+        const entity_nests = hass.states[entity_nests_id];
+
+        const entity_eggs_id = this.config.entity_eggs;
+        const entity_eggs = hass.states[entity_eggs_id];
+
+        let data_nests = entity_nests.state;
+        let data_eggs = entity_eggs.state;
 
         if (logging) {
-            console.log(nests_in_row)
+            console.log(nests_in_row);
             console.log(nests_in_column);
-            console.log(entity);
+            console.log(entity_nests);
+            console.log(entity_eggs);
+            console.log(data_nests);
+            console.log(data_eggs)
         }
 
-        let data = entity.state
-        if (logging)
-            console.log(data)
-
-        if ((!data.includes('o') && !data.includes('f')) || (data.length > 1 && !data.includes(separator))) {
-            this.content.innerHTML = `Entity id: ${entityId} - Waiting for data...`;
+        if ((!data_nests.includes('o') && !data_nests.includes('f')) || (data_nests.length > 1 && !data_nests.includes(separator))) {
+            this.content.innerHTML = `Entity id: ${entity_nests_id} - Waiting for data or bad format...`;
+            console.log("bad data Entity id:" + entity_nests_id)
+        }
+        if (!this.containsNumber(data_eggs) || this.containsLetters(data_eggs)) {
+            this.content.innerHTML = `Entity id: ${entity_eggs_id} - Waiting for data or bad format...`;
+            console.log("bad data Entity id:" + entity_eggs_id)
         } else {
-            let states = data.split(separator);
+            let states = data_nests.split(separator);
+            let counts = data_eggs.split(separator);
 
             let container = document.createElement("div");
             container.id = "nest-container";
@@ -42,7 +55,10 @@ class NestCard extends HTMLElement {
                 row.style.display = "flex";
                 for (let j = 0; j < nests_in_row; j++) {
                     let id = j + i * nests_in_row;
-                    console.log("nest - "+id)
+
+                    if (logging)
+                        console.log("nest - " + id)
+
                     const nest = document.createElement('div');
                     nest.style.width = "100px";
                     nest.style.height = "100px";
@@ -66,12 +82,35 @@ class NestCard extends HTMLElement {
                             nest.appendChild(chicken);
                         }
                     }
+
+                    if (id < counts.length) {
+                        const count = document.createElement('div');
+                        count.style.position = "absolute";
+                        count.style.left = "-10px";
+                        count.style.top = "72px";
+                        count.style.color = "black";
+                        count.style.width = "100%";
+                        count.style.textAlign = "right";
+                        count.style.fontSize = "20px";
+                        count.style.fontWeight = "bold";
+                        count.textContent = counts[id]
+                        nest.appendChild(count);
+                    }
                     row.appendChild(nest);
                 }
                 container.appendChild(row);
             }
             this.content.innerHTML = container.innerHTML;
         }
+    }
+
+
+    containsNumber(str) {
+        return str.match(/\d+/) !== null;
+    }
+
+    containsLetters(str) {
+        return str.match(/[a-zA-Z]/) !== null;
     }
 
     setConfig(config) {
@@ -83,4 +122,10 @@ class NestCard extends HTMLElement {
     }
 }
 
-customElements.define('nest-card', NestCard);
+customElements
+    .define(
+        'nest-card'
+        ,
+        NestCard
+    )
+;
