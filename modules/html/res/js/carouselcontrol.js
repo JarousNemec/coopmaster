88ff3cad24876cc3ce89e7f0e88946026f13dff8
JarousNemec <jarous.nemec@gmail.com>
2025-03-14 08:32:48 +0100

    working carousel pause it was because of asynchronous methods

--- modules/html/res/js/carouselcontrol.js
+++ modules/html/res/js/carouselcontrol.js
@@ -1,17 +1,17 @@
-$(function () {
+document.addEventListener ('DOMContentLoaded', () => {
 
-    $('#slider').on('slide.bs.carousel', function (event) {
-        // $('video').each(function () {
-        //     this.pause();
-        //     this.currentTime = 0;
-        //     $('#slider').carousel("cycle");
-        // });
+    $('#slider').on('slid.bs.carousel', function (event) {
+        $('video').each(function () {
+            this.pause();
+            this.currentTime = 0;
+            $('#slider').carousel("cycle");
+        });
 
         const activeSlide = event.relatedTarget;
 
         const videoElement = $(activeSlide).find('video')[0];
         if (videoElement) {
-            $('.carousel').carousel('pause')
+            $('#slider').carousel('pause');
             // $("#slider").carousel('pause');
             console.log("slider paused");
             videoElement.play().catch(error => {
@@ -21,32 +21,7 @@ $(function () {
         }
     });
 
-    // $('video').on('ended', function () {
-    //     $('#slider').carousel("cycle");
-    // });
+    $('video').on('ended', function () {
+        $('#slider').carousel("cycle");
+    });
 });
-
-// document.addEventListener('DOMContentLoaded', function () {
-//     const carouselElement = document.getElementById('slider');
-//     const videoElement = document.getElementById('carousel-video');
-//     console.log("test");
-//     // Pause the carousel when the video starts to play
-//     carouselElement.addEventListener('slide.bs.carousel', function (event) {
-//         var activeSlide = event.relatedTarget;
-//         console.log(activeSlide);
-//         if (activeSlide.id === 'video-slide') {
-//             $('#slider').carousel('pause'); // Pause the carousel
-//             videoElement.play();
-//         }
-//     });
-//
-//     // Resume the carousel when the video ends
-//     videoElement.addEventListener('ended', function () {
-//         $('#slider').carousel('cycle'); // Resume the carousel
-//     });
-//
-//     // Optionally pause the carousel if play button is directly clicked
-//     videoElement.addEventListener('play', function () {
-//         $('#slider').carousel('pause');
-//     });
-// });
